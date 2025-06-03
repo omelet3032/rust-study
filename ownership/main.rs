f25739b9e9e0afc3183cd04abe1cd80af6887996
omelet3032 <omelet3032@gmail.com>
2025-06-03 15:44:10 +0900

    ssc 프로젝트 소유권 해결 관련 시나리오 약식 코드 작성

---
+++ ownership/main.rs
@@ -0,0 +1,52 @@
+use std::fmt;
+
+fn main() {
+    
+    let country:Country = select_country();
+
+    let price:i32 = enter_stock_price(country);
+
+    print_result(price, country);
+}
+
+fn print_result(price:i32, country:Country) {
+    println!("print_result : {}, {}", price, country);
+
+}
+
+fn enter_stock_price(country:Country) -> i32 {
+    println!("enterstockprice에 입력되나? : {}", country);
+    return 100
+}
+
+fn select_country() -> Country {
+    println!("select_country 실행");
+    return Country::KR
+}
+
+impl fmt::Display for Country {
+    fn fmt(&self, f:&mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Country::KR => write!(f, "korea"),
+            Country::US => write!(f, "usa"),
+        }
+    }
+}
+
+impl fmt::Display for Message {
+    fn fmt(&self, f:&mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Message::ResultCountry(price, country) => write!(f, "가격 : {}  국가 선택 : {}", price, country),
+        }
+    }
+
+}
+
+enum Country {
+    KR,
+    US,
+}
+
+enum Message {
+    ResultCountry(i32, Country),
+}
